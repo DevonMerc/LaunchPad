f68a46d62635b4df093b6400f2a95aa59b25eb21
Shyee Meng <meng.shyee@gmail.com>
2024-06-04 11:34:29 -0400

    started frontend work

---
+++ vue/src/components/CampaignForm.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        My unorthodox comments VVV 
+        Thinking a form:
+        $ Goal
+        Title
+        Campaign manager
+        Description
+        image upload maybe
+        radio button for private or public
+        dropdown menu for tags set, or its like checkboxes? Gonna struggle with checkbox logic
+        submit button
+    </div>
+    <form>
+        <div>
+            <label for="goal">Whats the starting goal? </label>
+            <input type="number" id="goal" />
+        </div>
+        <div>
+            <label for="goal">Whats the starting goal? </label>
+            <input type="number" id="goal" />
+        </div>
+        <div>
+            <label for="goal">Whats the starting goal? </label>
+            <input type="number" id="goal" />
+        </div>
+        <div>
+            <label for="goal">Whats the starting goal? </label>
+            <input type="number" id="goal" />
+        </div>
+    </form>
+</template>
+
+<script>
+export default{
+
+}
+</script>
---
+++ vue/src/views/CreateCampaignView.vue
@@ -0,0 +1,16 @@
+<template>
+    <campaign-form/>
+</template>
+
+<script>
+import CampaignForm from '../components/CampaignForm.vue';
+
+export default{
+    components: {
+        CampaignForm
+    },
+    data(){
+        //dunno if well need this but whatever
+    }
+}
+</script>
