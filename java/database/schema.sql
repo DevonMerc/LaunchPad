132574bfca00d0867a6665e1f852ef236b57d3da
Adil Shad <adilshads15@gmail.com>
2024-06-04 16:59:48 -0400

    edr diagram

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -5,14 +5,14 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
-	first_name VARCHAR(100);
-	last_name VARCHAR(100);
-	user_email VARCHAR(150);
-	user_phone VARCHAR(15);
-	user_city VARCHAR(50);
-	user_state VARCHAR(50); NOT NULL
-	user_province VARCHAR(50); NOT NULL
-	user_country VARCHAR(50);
+	first_name VARCHAR(100),
+	last_name VARCHAR(100),
+	user_email VARCHAR(150),
+	user_phone VARCHAR(15),
+	user_city VARCHAR(50),
+	user_state VARCHAR(50),
+	user_province VARCHAR(50),
+	user_country VARCHAR(50),
 	Bio TEXT,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
