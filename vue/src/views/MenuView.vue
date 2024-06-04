5af41e3fdd8a3c77271fbe82366db5ddd637ffe2
Shyee Meng <meng.shyee@gmail.com>
2024-06-04 18:00:18 -0400

    New menu view works, still barebones in content, started minimal styling in SiteHeader

--- vue/src/views/MenuView.vue
+++ vue/src/views/MenuView.vue
@@ -1,6 +1,7 @@
 <template>
-    <button>Log in</button>
-    <button>Manage</button>
+    <!-- commented this out bc Manage redirects you to login anyway -->
+    <!-- <button @click="$router.push({name: 'login'})">Log in</button> -->
+    <button @click="$router.push({name: 'dashboard'})">Manage</button>
 </template>
 
 <script>
