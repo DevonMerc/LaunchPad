c65225ff8cba82758d797af4718244531f15e31b
Shyee Meng <meng.shyee@gmail.com>
2024-06-05 14:56:57 -0400

    small styling

--- java/database/schema.sql
+++ java/database/schema.sql
@@ -5,15 +5,6 @@ DROP TABLE IF EXISTS users;
 CREATE TABLE users (
 	user_id SERIAL,
 	username varchar(50) NOT NULL UNIQUE,
-	first_name VARCHAR(100),
-	last_name VARCHAR(100),
-	user_email VARCHAR(150),
-	user_phone VARCHAR(15),
-	user_city VARCHAR(50),
-	user_state VARCHAR(50),
-	user_province VARCHAR(50),
-	user_country VARCHAR(50),
-	Bio TEXT,
 	password_hash varchar(200) NOT NULL,
 	role varchar(50) NOT NULL,
 	CONSTRAINT PK_user PRIMARY KEY (user_id)
