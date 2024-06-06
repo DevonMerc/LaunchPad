7e92d0b19dbf9615b260be0cd8f88bc0161f533a
Iqra Shad <iqra.shad125@gmail.com>
2024-06-06 15:56:40 -0400

    view your campaign

---
+++ vue/src/components/ViewYourCampaigns.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+      <h6>Your Campaigns</h6>
+      <button type="button">Start A New Campaign</button>
+      <!--Need process to create individual cards for each campaign-->
+      <div v-if="campaign.pic">
+        <img src="../assets/PLACEHOLDER_LOGO.png" alt="Place Holder">
+      </div>
+      <h1>{{ campaign.title }}</h1>
+      <p>Description: {{ campaign.about }}</p>
+      <progress id="CampaignProgress" value="50" max="100"></progress>
+      <button type="button">Edit Campaign</button>
+      <button type="button">Delete Campaign</button>
+    </div>
+  </template>
+  
+  <script>
+  import { mapGetters } from 'vuex';
+  
+  export default {
+    computed: {
+      ...mapGetters(['campaign'])
+    }
+  };
+  </script>
+  
+  <style>
+  </style>
