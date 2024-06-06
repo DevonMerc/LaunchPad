3721009ad4acd645cb84359c8c28e60c53cf5f16
DevonMerc <devonvictoriaa@gmail.com>
2024-06-05 23:09:02 -0400

    backend campaign update part 2

--- java/src/main/java/com/techelevator/controller/CampaignController.java
+++ java/src/main/java/com/techelevator/controller/CampaignController.java
@@ -1,7 +1,12 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.Campaign;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 @CrossOrigin
@@ -18,4 +23,14 @@ public class CampaignController {
     //get individual campaign based on id
     //post individual campaign
     //put indiviudal campaign based on id
+
+
+//    List<Campaign> getPublicCampaigns(); //for home page
+//    List<Campaign> getCampaignsByManagerId(int id); //for dashboard
+//    List<Campaign> getCampaignsByDonorId(int id);
+//    List<Campaign> getCampaignsBySearch(String searchTerm); //for home page
+//    Campaign getCampaignById(int id);
+//    Campaign createCampaign(String title, LocalDate endDate, BigDecimal goal, int managerId, String imgURL, BigDecimal funding, boolean visibility, String description);
+//    boolean updateCampaign(String title, LocalDate endDate, BigDecimal goal, String imgURL, boolean visibility, String description, int id);
+
 }
