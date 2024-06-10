2ad4dcdf3b4c53c2eca48da711b2a4b8deaa9891
Adil Shad <adilshads15@gmail.com>
2024-06-10 16:07:33 -0400

    Login & Logout Buttons

--- vue/src/components/SiteHeader.vue
+++ vue/src/components/SiteHeader.vue
@@ -4,13 +4,40 @@
       <img src="../assets/Launchpad-logo-full.png" alt="LaunchPad" />
     </router-link>
     <button @click="$router.push({ name: 'menu' })" class="menu-button">Menu</button>
+    <button v-if="this.$store.state.token != ''" @click="logout" class="logout-button">
+      Logout
+    </button>
+    <button class="login" @click="this.$router.push({ name: 'createCampaign' })">
+      Login
+    </button>
+
+    
+    
+    <!-- <button class="logout-button">
+      <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">
+      Logout
+      </router-link>
+    </button> -->
+    <!-- <router-link v-bind:to="{ name: 'logout' }" v-if="this.$store.state.token != ''">
+      Logout
+    </router-link> -->
   </header>
 </template>
 
 <script>
+// export default {
+//   name: "SiteHeader"
+// };
+
 export default {
-  name: "SiteHeader"
+  name: "SiteHeader",
+  methods: {
+    logout() {
+      this.$router.push({ name: 'logout' });
+    }
+  }
 };
+
 </script>
 
 <style scoped>
@@ -38,6 +65,29 @@ export default {
   border-radius: 4px;
 }
 
+.logout-button {
+  background-color: rgb(228, 225, 225);
+  color: black;
+  font-weight: bolder;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  border: none;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border-radius: 4px;
+  text-decoration: none;
+}
+
+.login{
+  background-color: rgb(228, 225, 225);
+  color: black;
+  font-weight: bolder;
+  font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  border: none;
+  padding: 0.5rem 1rem;
+  font-size: 1rem;
+  border-radius: 4px;
+  text-decoration: none;
+}
 @media (min-width: 600px) {
   .site-header {
     padding: 1rem 2rem;
