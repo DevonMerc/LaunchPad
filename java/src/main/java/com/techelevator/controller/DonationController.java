faff031dfc2c5d9e4b2fcabc33151c44804e4034
Shyee Meng <meng.shyee@gmail.com>
2024-06-12 15:19:09 -0400

    principal fixes, removed campaign organizer input from form

--- java/src/main/java/com/techelevator/controller/DonationController.java
+++ java/src/main/java/com/techelevator/controller/DonationController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 @CrossOrigin
 @RequestMapping(path = "/donations")
-//@PreAuthorize("isAuthenticated()")
+@PreAuthorize("isAuthenticated()")
 public class DonationController {
     private DonationDao donationDao;
 
